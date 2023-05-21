950eafe532b6c2712a62fdb976b89d89ea0710c0
Fernando Garcia <72968022@continental.edu.pe>
2023-05-20 23:04:39 -0500

    Segunda Pagina con algunos errores

--- src/Controlador/MainApp.java
+++ src/Controlador/MainApp.java
@@ -3,18 +3,45 @@ package Controlador;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import Modelo.Person;
+import Vista.PersonOverviewController;
 
 public class MainApp extends Application {
-
+    //
     private Stage primaryStage;
     private BorderPane rootLayout;
+    //
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+    public MainApp() {
+		// Add some sample data
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Meier"));
+		personData.add(new Person("Martin", "Mueller"));
+	}
+  
+	/**
+	 * Returns the data as an observable list of Persons. 
+	 * @return
+	 */
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
 
     @Override
+    
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
@@ -50,14 +77,19 @@ public class MainApp extends Application {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/Vista/PersonOverview.fxml"));
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
-            
+    
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
+    
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+    
         } catch (IOException e) {
             e.printStackTrace();
-        }   
+        }
     }
     
 	/**
